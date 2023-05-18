563635ff86caf321d25b0468e00cbf45d886b354
Rhett Muse <rhett.muse123@gmail.com>
2023-05-18 15:30:53 -0500

    add mock fish data to database

--- database.js
+++ database.js
@@ -7,7 +7,25 @@ const database = {
             image: "#",
             length: "15cm",
             location: "Bahamas",
-
-        }
+            species: "gold fish",
+        },
+        {
+            name: "Kevin",
+            food: "cytoplankton",
+            size: 1,
+            image: "#",
+            length: "10cm",
+            location: "Gulf of Mexico",
+            species: "sunfish",
+        },
+        {
+            name: "Sandy",
+            food: "schooling fish",
+            size: 4,
+            image: "#",
+            length: "35cm",
+            location: "San Francisco bay",
+            species: "shark",
+        },
     ]
 }
